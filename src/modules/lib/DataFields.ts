a361f676dcc20745d3679b6eb747b10bb471ae72
atz3n <A_Aldemir@hotmail.de>
2019-12-17 16:25:21 +0100

    added license notices

--- src/modules/lib/DataFields.ts
+++ src/modules/lib/DataFields.ts
@@ -1,7 +1,23 @@
+/*
+ *  Copyright (C) 2019  Attila Aldemir <a_aldemir@hotmail.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { account } from '@somedotone/ardor-ts';
 import { ACCOUNT_PREFIX, DATA_FIELD_SEPARATOR, DUMMY_ACCOUNT_RS, MAX_PAYLOAD_LENGTH, noError, NUMBER_OF_DATA_FIELDS, PROTOCOL_IDENTIFIER, PROTOCOL_VERSION } from '../../constants';
 import { EntityType, Error, ErrorCode, State } from '../../types';
-
 
 
 enum DataField {
